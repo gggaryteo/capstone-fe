c9df0e243c4c577f1b8a55256b303c95600dd782
gggaryteo <garyteoprograms@gmail.com>
2023-02-13 10:44:25 +0800

    redirect to dashboard after signing up

--- src/Components/RegisterForm/RegisterForm.js
+++ src/Components/RegisterForm/RegisterForm.js
@@ -85,7 +85,7 @@ const RegisterForm = ( { onError }) => {
     if (isNotNull) {
       userSignUp({ ...allUserParams, online: true })
           .then(setAuthState)
-          .then(() => navigate("/"))
+          .then(() => navigate("/dashboard"))
           .catch(onError)
           
       setIsPost(false);
