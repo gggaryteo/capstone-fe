137d3807c7a47008c4695734d0db550f88bc1026
gggaryteo <garyteoprograms@gmail.com>
2023-02-16 17:26:23 +0800

    changed classname from icon to icon-chat

--- src/Pages/Chat/StatusIcon.js
+++ src/Pages/Chat/StatusIcon.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./StatusIcon.css";
 
 export default function StatusIcon(props) {
-  let classname = props.isOnline ? "icon connected" : "icon";
+  let classname = props.isOnline ? "icon connected" : "icon-chat";
 
   return <i className={classname}></i>;
 }
