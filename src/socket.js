2341c6b4143b8380240e9f383f1e46a83ceb4b73
lazyhazydaze <juruissic@gmail.com>
2023-02-17 22:36:41 +0800

    edit backend URL

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000"; //backend URL, rmb to put in .env before pushing
+const URL = "http://localhost:3001"; //backend URL, rmb to put in .env before pushing
 const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
-  console.log("printing whatever")
+  console.log("printing whatever");
   console.log(event, args);
 });
 
